50cd5ef8b5fd221cc7c32746da597e8d3492148c
amine <mohamedaminerafai@gmail.com>
2020-03-11 13:49:56 +0100

    sql srv11

--- tests/travis/install-mssql.sh
+++ tests/travis/install-mssql.sh
@@ -5,9 +5,10 @@ set -ex
 echo Installing driver dependencies
 
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
-sudo apt-get update
-ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 unixodbc unixodbc-dev libssl1.0.0
+echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic main" | sudo tee /etc/apt/sources.list.d/mssql-release.list
+sudo apt update
+sudo apt install msodbcsql17
+#ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 unixodbc unixodbc-dev libssl1.0.0
 
 echo Setting up Microsoft SQL Server
 
