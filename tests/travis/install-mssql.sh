f62d0e73e5723ac5f8c338d8b4d515bf5de97d03
amine <mohamedaminerafai@gmail.com>
2020-03-11 10:26:46 +0100

    sqls cr

--- tests/travis/install-mssql.sh
+++ tests/travis/install-mssql.sh
@@ -2,43 +2,24 @@
 
 set -ex
 
-echo Installing drivers
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/14.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
-sudo apt-get update
-ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 mssql-tools unixodbc libssl1.0.0
+echo Installing driver dependencies
 
-#curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-#echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic main" | sudo tee /etc/apt/sources.list.d/mssql-release.list
-#sudo apt update
-#sudo apt install -qy msodbcsql17 mssql-tools unixodbc libssl1.0.0
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql.list
+sudo apt-get update
+ACCEPT_EULA=Y sudo apt-get install -qy msodbcsql17 unixodbc unixodbc-dev libssl1.0.0
 
 echo Setting up Microsoft SQL Server
 
-#sudo docker pull microsoft/mssql-server-linux:2017-latest
-sudo docker pull mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+sudo docker pull microsoft/mssql-server-linux:2017-latest
 sudo docker run \
     -e 'ACCEPT_EULA=Y' \
-    -e 'SA_PASSWORD=Anonymizer2018' \
+    -e 'SA_PASSWORD=Doctrine2018' \
     -p 127.0.0.1:1433:1433 \
-    --name db \
+    --name mssql \
     -d \
-    #microsoft/mssql-server-linux:2017-latest
-    mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
+    microsoft/mssql-server-linux:2017-latest
 
-retries=20
-until (echo quit | /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -l 1 -U sa -P Anonymizer2018 &> /dev/null)
-do
-    if [[ "$retries" -le 0 ]]; then
-        echo SQL Server did not start
-        exit 1
-    fi
-
-    retries=$((retries - 1))
-
-    echo Waiting for SQL Server to start...
-
-    sleep 5s
-done
+sudo docker exec -i mssql bash <<< 'until echo quit | /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -l 1 -U sa -P Doctrine2018 > /dev/null 2>&1 ; do sleep 1; done'
 
 echo SQL Server started
