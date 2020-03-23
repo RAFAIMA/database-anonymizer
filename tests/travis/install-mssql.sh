406995fe3d5483493cb4d745a85cd7cf65d2648e
amine <mohamedaminerafai@gmail.com>
2020-03-23 11:46:15 +0100

    testing only Sql Server4

--- tests/travis/install-mssql.sh
+++ tests/travis/install-mssql.sh
@@ -6,14 +6,7 @@ echo Setting up Microsoft SQL Server
 
 sudo docker pull microsoft/mssql-server-linux:2017-latest
 #sudo docker pull mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
-sudo docker run \
-    -d \
-    microsoft/mssql-server-linux:2017-latest
-    -e 'ACCEPT_EULA=Y' \
-    -e 'SA_PASSWORD=Anonymizer2018' \
-    -p 127.0.0.1:1433:1433 \
-    --name db \
-
+sudo docker run -e 'ACCEPT_EULA=Y'  -e 'SA_PASSWORD=Anonymizer2018' -p 127.0.0.1:1433:1433 --name db -d microsoft/mssql-server-linux:2017-latest
     #mcr.microsoft.com/mssql/server:2019-GA-ubuntu-16.04
 
 retries=20
